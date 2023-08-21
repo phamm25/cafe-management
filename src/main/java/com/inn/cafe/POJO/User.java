e5ee2784936c4a4fa7e31fe8f257cd76d47bdfb9
Pham Anh Minh <anhminh23252001@gmail.com>
2023-08-21 20:17:18 +0700

    package

--- src/main/java/com/inn/cafe/POJO/User.java
+++ src/main/java/com/inn/cafe/POJO/User.java
@@ -1,4 +1,13 @@
 package com.inn.cafe.POJO;
 
+import jakarta.persistence.Entity;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+
+@Entity
+@DynamicUpdate
+@DynamicInsert
 public class User {
 }
